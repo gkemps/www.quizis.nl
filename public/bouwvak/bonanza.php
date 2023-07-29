651b69d1dff78dfcfd4b2c1830eb91aac519fb8b
geert <geert@route42.nl>
2023-07-29 08:09:36 +0200

    bouwvak zitter instructie

---
+++ public/bouwvak/bonanza.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bouwvak Bakstenen Bonanza</title>
+  <!-- Bootstrap 5 CSS Link -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
+    <div class="row">
+      <div class="col-12">
+        <h1>Bouwvaktoernooi Bakstenen Bonanza!!</h1>
+        <ul>
+            <li>Wedstrijd afgelopen? Tijd voor Bouwvak Bakstenen Bonanza!!</li>
+            <li>Laat ieder dubbelteam (dus niet iedere persoon!) 1 baksteen kiezen uit de stapel</li>
+            <li>Voor het nummer in <a href="/bouwvak/number_check.php">hier</a></li>
+            <li>Prijs gewonnen?</li>
+            <ul>
+                <li>Ja: gefeliciteerd! Neem je prijs in ontvangst bij de prijzentafel.<br />
+                    Alleen de prijs die correspondeert met je nummer, <b><u>niet ruilen</b></u>!!
+</li>
+                <li>Nee: helaas ... maar volgende wedstrijd weer een kans!
+            </ul>
+            <li>Leg de bakstenen terug op de pallet aan de zijkant van het terras.<br />
+                Dus <u>niet</u> terug op de stapel</li>
+        </ul>
+
+        <h3>Prijzen</h3>
+        <ul>
+            <li>Hoofdprijs: 1x 2 kaatjes ABN Amro tennistoernooi</li>
+            <li>Tennisdirect prijzen: Bjorn Borg caps, Shirts, Handdoeken</li>
+            <li>Diverse kleinere prijzen: met oa BBQ accessoires, klus spullen, (zomer) gadgets</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap 5 JS and Popper.js links (required for Bootstrap features) -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- public/bouwvak/index.php
+++ public/bouwvak/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Simple Form</title>
+  <title>Bouwvak 2023</title>
   <!-- Bootstrap 5 CSS Link -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -13,35 +13,40 @@
   <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
     <div class="row">
       <div class="col-12">
-        <h3>Bouwvak Bakstenen Bonanza!!</h3>
-        <?php if(isset($_GET['prize'])): ?>
-          <?php if (empty($_GET['prize']) || $_GET['prize'] == "helaas"): ?>
-            <div class="alert alert-danger" role="alert">
-              Er is helaas geen prijs voor nummer <?php echo $_GET['number']; ?>
-            </div>
-            <div>
-              <a href="https://www.quizis.nl/bouwvak">Terug</a>
-            </div>
-          <?php else: ?>
-            <div class="alert alert-success" role="alert">
-              Gewonnen prijs: <u><?php echo $_GET['prize']; ?></u>.<br/><br/>Gefeliciteerd!
-            </div>
-            <div>
-              <small><i>Prijs uitreiken bij prijstafel. Alleen bovenstaande prijs, niet ruilen voor iets anders!</i></small>
-            </div>
-            <div>
-              <a href="https://www.quizis.nl/bouwvak">Terug</a>
-            </div>
-          <?php endif ?>
-        <?php else: ?>
-          <form action="check.php" method="post">
-            <div class="mb-3">
-              <label for="number" class="form-label">Baksteen nummer:</label>
-              <input type="text" class="form-control" name="number" id="number" placeholder="voer hier nummer in" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Prijs?</button>
-          </form>
-        <?php endif ?>
+        <h1>Bouwvaktoernooi 2023</h1>
+        <h3>Voor aanvang van de wedstrijd</h3>
+        <ul>
+            <li>Heet deelnemers welkom op Hans van Kroonenburg Bouwvaktoernooi 2023!</li>
+            <li>Vraag aan deelnemers of dit hun eerste wedstrijd is</li>
+            <ul>
+                <li>Zo ja: geef ze welkomstgeschenken (voucher, gripje & TCO demper)</li>
+                <li>Zo nee: check of ze dat al gekregen hebben</li>
+            </ul>
+            <li>Deelnemers op "aanwezig" zetten in Toernooiplanner</li>
+            <ul>
+                <li>Toernooiplanner vind je als snelkoppeling op dekstop met "Tennis Toernoi Planner"</li>
+                <li>email: toernooi@tenniscluboirschot.nl password: Tennisclub</li>
+                <li><a href="/bouwvak/handleiding.pdf" target="_blank">Handleiding</a></li>
+            </ul>
+            <li>Check wanneer ze moeten spelen op in Toernooiplanner</li>
+            <li>Indien baan beschikbaar en tegenstanders al aanwezig kunnen ze beginnen</li>
+            <li>Baan toekennen en wedstrijd starten in Toernooiplanner</li>
+            <li>Ballen meegeven</li>
+            <li>Mochten deelnemers vragen hebben mbt verzetten overige wedstrijden:
+                <br />graag <u>altijd</u> eerst contact opnemem met Robert, zie onderaan bij contact</li>
+        </ul>
+        <h3>Na afloop van de wedstrijd</h3>
+        <ul>
+            <li>Ballen weer terug aannemen</li>
+            <li>Wedstrijd afronden en uitslag invoeren in toernooi planner</li>
+            <li>Deelnemers mee laten doen aan <a href="/bouwvak/bonanza.php">Bouwvak Bakstenen Bonanza!</a></li>
+            <li>Aan het einde van de ronde de uitslagen publiceren</li>
+        </ul>
+        <h3>Contact</h3>
+        <ul>
+            <li>Vragen over wedstrijd planning: Robert Goutziers (06-53646012)</li>
+            <li>Vragen over zitschema/bar/openen&sluiten: Geert Kemps (06-48333849)
+        </ul>
       </div>
     </div>
   </div>
