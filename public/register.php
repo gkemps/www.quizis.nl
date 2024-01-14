07ca17194ee3aa2994037738fea4846e39505a93
Geert K <geert@pipple.nl>
2024-01-14 13:54:17 +0100

    remove redundant scripts

--- public/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (empty($_POST['teamname']) || empty($_POST['teamsize'])) {
-    header("location: https://www.quizis.nl/er-ging-iets-mis.html");
-}
-
-$amount = 3 * (int) $_POST['teamsize'];
-$desc = rawurlencode($_POST['teamname']);
-
-header("location: ".sprintf("https://bunq.me/quiz/%d/%s", $amount, $desc));
