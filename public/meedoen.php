c09c447d6fd27d1f1b26b15ea2b978c9f0dd3cc5
Geert K <geert@pipple.nl>
2024-01-14 13:54:52 +0100

    send default confirmation m ail

--- public/meedoen.php
+++ public/meedoen.php
@@ -1,5 +1,10 @@
 <?php
 include "connect.php";
+require '../vendor/autoload.php';
+require '../secrets.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 if (empty($_POST['name'])) {
     header('Location: http://www.quizis.nl/er-ging-iets-mis.html');
@@ -12,6 +17,7 @@ $email = $conn->real_escape_string($_POST['email']);
 $quizId = $conn->real_escape_string($_POST['quizId']);
 $guid = substr(com_create_guid(), 1, 36);
 
+//save subscription
 $sql = "INSERT INTO quiz_Team (name, captain, email, quiz_quiz_id, teamId, dateCreated)
 VALUES ('{$teamname}', '{$teamcaptain}', '{$email}', $quizId, '{$guid}', NOW())";
 
@@ -19,21 +25,128 @@ if (!$conn->query($sql)) {
     die("Oeps, er ging iets fout. Probeer het later nog een keer.");
 }
 
+// fetch id
 $sql = "SELECT LAST_INSERT_ID() AS id";
 $result = $conn->query($sql);
 if (!$result) {
     die("Oeps, er ging iets fout. Probeer het later nog een keer.");
 }
 
+// fetch subscription
 $sql = "SELECT * FROM quiz_Team WHERE id = {$result->fetch_assoc()['id']}";
-$subscription = $conn->query($sql);
+$result = $conn->query($sql);
 if (!$result) {
     die("Oeps, er ging iets fout. Probeer het later nog een keer.");
+}
+$subscription = $result->fetch_assoc();
+
+// fetch quiz
+$sql = "SELECT * FROM quiz_Quiz WHERE id = {$quizId}";
+$result = $conn->query($sql);
+if (!$result) {
+    die("Oeps, er ging iets fout. Probeer het later nog een keer.");
+}
+$quiz = $result->fetch_assoc();
+
+// fetch location
+$sql = "SELECT * FROM quiz_Location WHERE id = {$quiz['quiz_Location_id']}";
+$result = $conn->query($sql);
+if (!$result) {
+    die("Oeps, er ging iets fout. Probeer het later nog een keer.");
+}
+$location = $result->fetch_assoc();
+
+// quiz specifications
+$payPerTeam = !empty($quiz['pricePerTeam']);
+$payPerPerson = !empty($quiz['pricePerPerson']);
+$maxTeamSize = !empty($quiz['maxTeamSize']) ? $quiz['maxTeamSize'] : 5;
+$prepay = !empty($quiz['prepay']);
+$amount = $payPerTeam ? $quiz['pricePerTeam'] : ($payPerPerson ? $quiz['pricePerPerson'] * $maxTeamSize : 0);
+
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
+$url = $protocol.'://'.$_SERVER['HTTP_HOST'] . "/bedankt/{$subscription['teamId']}";
+
+//payment text
+$paymentText = "";
+if ($prepay) {
+    if ($payPerTeam) {
+        $paymentText = "Deelname is {$amount} euro per team en mocht je dat nog niet hebben voldaan, 
+        dan mag dat via <a href=\"{$url}\">deze link</a>. Je ontvangt een definitie bevestiging zodra 
+        je betaling is ontvangen";
+    } else if ($payPerPerson) {
+        $paymentText = "Deelname is {$amount} euro per team ({$quiz['pricePerPerson']} euro pp) en mocht 
+        je dat nog niet hebben voldaan, dan mag dat via <a href=\"{$url}\">deze link</a>. Je ontvangt een 
+        definitie bevestiging zodra je betaling is ontvangen";
+    }
+} else {
+    if ($payPerTeam) {
+        $paymentText = "Deelname is {$amount} euro per team en mocht je dat nog niet hebben voldaan, 
+        dan mag dat via <a href=\"{$url}\">deze link</a>. Betalen op de avond zelf mag ook, maar vooraf 
+        betalen wordt gewaardeerd.";
+    } else if ($payPerPerson) {
+        $paymentText = "Deelname is {$amount} euro per team ({$quiz['pricePerPerson']} euro pp) en mocht 
+        je dat nog niet hebben voldaan, dan mag dat via <a href=\"{$url}\">deze link</a>. Betalen op de avond
+        zelf mag ook, maar vooraf betalen wordt gewaardeerd.<br />
+        <br />
+        Later mensen nog toevoegen aan je team? Ook dat kan via <a href=\"{$url}\">deze link</a>.";
+    }
+}
+
+// date formating
+$date = new DateTime($quiz['date']);
+$formatter = new \IntlDateFormatter('nl_NL', \IntlDateFormatter::SHORT, \IntlDateFormatter::SHORT);
+$formatter->setPattern('EEEE d LLLL HH:mm');
+
+//send mail
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->Host = 'pixel.mxrouting.net';
+$mail->SMTPAuth = true;
+$mail->Username = 'no-reply@quizis.nl';
+$mail->Password = SMTP_PASSWORD;
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+$mail->Port = 465;
+$mail->isHTML(true);
+$mail->CharSet = 'UTF-8';
+$mail->Encoding = 'base64';
+$mail->Subject = 'Inschrijving Quizis Pubquiz';
+try {
+    $mail->setFrom('no-reply@quizis.nl', 'Quizis');
+    $mail->addReplyTo('info@quizis.nl', 'Quizis');
+} catch(Exception $e) {
+    die('error setting phpmailer addresses: '.$e->getMessage());
+}
+$message = file_get_contents("templates/confirmation.html");
+$message = str_replace("*|QUIZ|*", $quiz['name'], $message);
+$message = str_replace("*|WHEN|*", $formatter->format($date). " uur @ " . $location['name'], $message);
+$message = str_replace("*|PAYMENTTEXT|*", $paymentText, $message);
+$message = str_replace("*|TEAMNAME|*", $subscription['name'], $message);
+$message = str_replace("*|TEAMCAPTAIN|*", $subscription['captain'], $message);
+$message = str_replace("*|MAXTEAMSIZE|*", $maxTeamSize, $message);
+
+try {
+    $mail->clearAddresses();
+    $mail->addAddress($subscription['email']);
+    $mail->Body = $message;
+    $mail->AltBody = sprintf(
+        "Bedankt voor jullie inschrijving als team %s voor %s op %s. Check of jullie betaling al in goede orde is ontvangen via deze link: {$url}", 
+        $subscription['name'], 
+        $quiz['name'],
+        $formatter->format($date). " uur"
+    );
+
+    $mailResult = $mail->send();
+    if ($mailResult) {
+        $sqlUpdate = "UPDATE quiz_Team SET paid = 0 where id = ".$subscription['id'];
+        $updateResult = $conn->query($sqlUpdate);
+    }
+} catch (Exception $e) {
+    // @todo log error
 }
 
 $conn->close();
 
 $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
-$url = $protocol.'://'.$_SERVER['HTTP_HOST'] . "/bedankt/{$subscription->fetch_assoc()['teamId']}";
+$url = $protocol.'://'.$_SERVER['HTTP_HOST'] . "/bedankt/{$subscription['teamId']}";
 
 header("Location: $url");
