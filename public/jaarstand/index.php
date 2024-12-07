9f41955382e1e813699bfa4f03baa65c2c84f14d
Geert K <geert@pipple.nl>
2024-12-07 20:36:34 +0100

    jaarstand nov 2024

---
+++ public/jaarstand/index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pub Quiz Jaarstand</title>
+    <link rel="stylesheet" href="/jaarstand/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>De Gezellige Pubquiz 2024 - Jaarstand</h1>
+        <p><i>(bijgewerkt tot en met november)</i></p>
+    </header>
+    <main>
+        <!-- Summary Cards -->
+        <section id="summary">
+            <div class="card">
+                <h2>Teams</h2>
+                <p id="total-teams">
+                    160
+                </p>
+            </div>
+            <div class="card">
+                <h2>Quizzers</h2>
+                <p id="paid-teams">
+                    643
+                </p>
+            </div>
+            <div class="card">
+                <h2>Vragen gesteld</h2>
+                <p id="total-members">
+                    720
+                </p>
+            </div>
+            <div class="card">
+                <h2>Punten gehaald</h2>
+                <p id="revenue">
+                    6236,5
+                </p>
+            </div>
+        </section>
+
+        <!-- Teams Table -->
+        <section id="teams-overview">
+            <p>
+                <i style="z-index: 100">Teams met 4 deelnames of meer. Puntentelling op basis van Eurovisie
+                    principe. Meedoen is minimaal 1 punt. Slechtste score wordt weggestreept.</i>
+            </p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nr</th>
+                        <th>Teamnaam</th>
+                        <th>Deelnames</th>
+                        <th>Gewonnen</th>
+                        <th>Punten</th>
+                    </tr>
+                </thead>
+                <tbody id="teams-table">
+                    <tr>
+                        <td>1.</td>
+                        <td>Kwis Um</td>
+                        <td>9</td>
+                        <td>1</td>
+                        <td>69</td>
+                    </tr>
+                    <tr>
+                        <td>2.</td>
+                        <td>De Stille Willies</td>
+                        <td>8</td>
+                        <td>1</td>
+                        <td>57</td>
+                    </tr>
+                    <tr>
+                        <td>3.</td>
+                        <td>Skiete Willies</td>
+                        <td>5</td>
+                        <td>4</td>
+                        <td>55</td>
+                    </tr>
+                    <tr>
+                        <td>4.</td>
+                        <td>Kwisblok</td>
+                        <td>9</td>
+                        <td>2</td>
+                        <td>53</td>
+                    </tr>
+                    <tr>
+                        <td>5.</td>
+                        <td>Old & Wise</td>
+                        <td>8</td>
+                        <td></td>
+                        <td>44</td>
+                    </tr>
+                    <tr>
+                        <td>6.</td>
+                        <td>Uit De Bocht</td>
+                        <td>7</td>
+                        <td></td>
+                        <td>41</td>
+                    </tr>
+                    <tr>
+                        <td>7.</td>
+                        <td>Tantaluskwelling</td>
+                        <td>4</td>
+                        <td>1</td>
+                        <td>35</td>
+                    </tr>
+                    <tr>
+                        <td>8.</td>
+                        <td>Neighbours From Hell</td>
+                        <td>6</td>
+                        <td></td>
+                        <td>34</td>
+                    </tr>
+                    <tr>
+                        <td>9.</td>
+                        <td>Hutje Op De Hei</td>
+                        <td>7</td>
+                        <td></td>
+                        <td>30</td>
+                    </tr>
+                    <tr>
+                        <td>10.</td>
+                        <td>Oirschotse Mie-en</td>
+                        <td>9</td>
+                        <td></td>
+                        <td>9</td>
+                    </tr>
+                    <tr>
+                        <td>11.</td>
+                        <td>Allegaartje</td>
+                        <td>6</td>
+                        <td></td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>12.</td>
+                        <td>Lekker Met De Meiden</td>
+                        <td>6</td>
+                        <td></td>
+                        <td>6</td>
+                    </tr>
+                    <tr>
+                        <td>13.</td>
+                        <td>Bier Is Belangrijker Dan Winnen</td>
+                        <td>5</td>
+                        <td></td>
+                        <td>5</td>
+                    </tr>
+                    <tr>
+                        <td>14.</td>
+                        <td>Quiz Of The Stone Age</td>
+                        <td>4</td>
+                        <td></td>
+                        <td>5</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+
+    </main>
+
+</body>
+
+</html>
